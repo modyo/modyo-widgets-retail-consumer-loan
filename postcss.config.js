9912df9caa5aa9abd21be49d4f63638e63dc52e9
Johann Rojas <jrojas@modyo.com>
2021-07-16 12:41:36 -0500

    fix: update postcss rule to show vue slider

--- postcss.config.js
+++ postcss.config.js
@@ -1,31 +1,27 @@
-/* eslint-disable global-require */
-const IN_PRODUCTION = process.env.NODE_ENV === 'production';
-const plugins = {};
+const PURGE_CSS = require('@fullhuman/postcss-purgecss');
 
-if (IN_PRODUCTION) {
-  plugins.postcss = require('@fullhuman/postcss-purgecss')({
-    content: ['./public/**/*.html', './src/**/*.vue'],
-    defaultExtractor(content) {
-      const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
-      return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
-    },
-    whitelist: [],
-    whitelistPatterns: [
-      /-(leave|enter|appear)(|-(to|from|active))$/,
-      /^(?!(|.*?:)cursor-move).+-move$/,
-      /^router-link(|-exact)-active$/,
-      /data-v-.*/,
-      /svg.*/,
-      /fa.*/,
-      /^d-*/,
-      /^multiselect*/,
-      /^vue-slider*/,
-    ],
-  });
-}
-
-plugins.autoprefixer = {};
+const AUTOPREFIXER = require('autoprefixer');
 
 module.exports = {
-  plugins,
+  plugins:
+  process.env.NODE_ENV === 'production'
+    ? [AUTOPREFIXER, PURGE_CSS({
+      content: ['./public/**/*.html', './src/**/*.vue'],
+      defaultExtractor(content) {
+        const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
+        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
+      },
+      safelist: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+        /data-v-.*/,
+        /svg.*/,
+        /fa.*/,
+        /^d-*/,
+        /^multiselect*/,
+        /^vue-slider*/,
+      ],
+    })]
+    : [AUTOPREFIXER],
 };
